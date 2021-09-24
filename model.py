c6c995da8836e26793c5fa849ca60ab23ca82fac
hogansung <wrangle1005@gmail.com>
2021-09-23 23:08:17 -0700

    Style changes

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torchvision.models as models
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
-        super(EncoderCNN, self).__init__()
+        super().__init__()
         resnet = models.resnet50(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
@@ -32,24 +32,30 @@ class DecoderRNN(nn.Module):
 
         self.word_embeddings = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(
-            input_size=embed_size,
-            hidden_size=hidden_size,
-            num_layers=num_layers,
+            input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers,
         )
         self.fc = nn.Linear(hidden_size, vocab_size)
 
     def forward(self, features, captions):
-        assert features.shape[0] == captions.shape[0], "Batch sizes are differrent for features and captions."
+        assert (
+            features.shape[0] == captions.shape[0]
+        ), "Batch sizes are different for features and captions."
         device = features.get_device()
 
         # ignore the last caption, which is <END>
-        inputs = torch.cat((features.view(-1, 1, self.embed_size), self.word_embeddings(captions[:,:-1])), dim=1)
-#         print(inputs.shape)
+        inputs = torch.cat(
+            (
+                features.view(-1, 1, self.embed_size),
+                self.word_embeddings(captions[:, :-1]),
+            ),
+            dim=1,
+        )
+        # print(inputs.shape)
 
         h0 = torch.randn(self.num_layers, inputs.shape[1], self.hidden_size).to(device)
         c0 = torch.randn(self.num_layers, inputs.shape[1], self.hidden_size).to(device)
         outputs, hidden = self.lstm(inputs, (h0, c0))
-#         print(outputs.shape)
+        # print(outputs.shape)
 
         return self.fc(outputs)
 
--- vocabulary.py
+++ vocabulary.py
@@ -1,19 +1,22 @@
-import nltk
-import pickle
 import os.path
-from pycocotools.coco import COCO
+import pickle
 from collections import Counter
 
-class Vocabulary(object):
+import nltk
+from pycocotools.coco import COCO
 
-    def __init__(self,
+
+class Vocabulary(object):
+    def __init__(
+        self,
         vocab_threshold,
-        vocab_file='./vocab.pkl',
+        vocab_file="./vocab.pkl",
         start_word="<start>",
         end_word="<end>",
         unk_word="<unk>",
-        annotations_file='../cocoapi/annotations/captions_train2014.json',
-        vocab_from_file=False):
+        annotations_file="../cocoapi/annotations/captions_train2014.json",
+        vocab_from_file=False,
+    ):
         """Initialize the vocabulary.
         Args:
           vocab_threshold: Minimum word count threshold.
@@ -32,54 +35,50 @@ class Vocabulary(object):
         self.unk_word = unk_word
         self.annotations_file = annotations_file
         self.vocab_from_file = vocab_from_file
+        self.word2idx = {}
+        self.idx2word = {}
+        self.word_idx = 0
         self.get_vocab()
 
     def get_vocab(self):
         """Load the vocabulary from file OR build the vocabulary from scratch."""
         if os.path.exists(self.vocab_file) & self.vocab_from_file:
-            with open(self.vocab_file, 'rb') as f:
+            with open(self.vocab_file, "rb") as f:
                 vocab = pickle.load(f)
                 self.word2idx = vocab.word2idx
                 self.idx2word = vocab.idx2word
-            print('Vocabulary successfully loaded from vocab.pkl file!')
+            print("Vocabulary successfully loaded from vocab.pkl file!")
         else:
             self.build_vocab()
-            with open(self.vocab_file, 'wb') as f:
+            with open(self.vocab_file, "wb") as f:
                 pickle.dump(self, f)
-        
+
     def build_vocab(self):
         """Populate the dictionaries for converting tokens to integers (and vice-versa)."""
-        self.init_vocab()
         self.add_word(self.start_word)
         self.add_word(self.end_word)
         self.add_word(self.unk_word)
         self.add_captions()
 
-    def init_vocab(self):
-        """Initialize the dictionaries for converting tokens to integers (and vice-versa)."""
-        self.word2idx = {}
-        self.idx2word = {}
-        self.idx = 0
-
     def add_word(self, word):
         """Add a token to the vocabulary."""
-        if not word in self.word2idx:
-            self.word2idx[word] = self.idx
-            self.idx2word[self.idx] = word
-            self.idx += 1
+        if word not in self.word2idx:
+            self.word2idx[word] = self.word_idx
+            self.idx2word[self.word2idx] = word
+            self.word_idx += 1
 
     def add_captions(self):
         """Loop over training captions and add all tokens to the vocabulary that meet or exceed the threshold."""
         coco = COCO(self.annotations_file)
         counter = Counter()
-        ids = coco.anns.keys()
-        for i, id in enumerate(ids):
-            caption = str(coco.anns[id]['caption'])
+        annotation_indices = coco.anns.keys()
+        for idx, annotation_index in enumerate(annotation_indices):
+            caption = str(coco.anns[idx]["caption"])
             tokens = nltk.tokenize.word_tokenize(caption.lower())
             counter.update(tokens)
 
-            if i % 100000 == 0:
-                print("[%d/%d] Tokenizing captions..." % (i, len(ids)))
+            if idx % 100000 == 0:
+                print("[%d/%d] Tokenizing captions..." % (idx, len(annotation_index)))
 
         words = [word for word, cnt in counter.items() if cnt >= self.vocab_threshold]
 
@@ -87,7 +86,7 @@ class Vocabulary(object):
             self.add_word(word)
 
     def __call__(self, word):
-        if not word in self.word2idx:
+        if word not in self.word2idx:
             return self.word2idx[self.unk_word]
         return self.word2idx[word]
 
